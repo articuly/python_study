38f5599f3546f00c7bee3fc4f093b9ec0fc0fa1d
articuly <articuly@gmail.com>
2019-12-22 12:48:31 +0800

    补充作业

--- 补充练习/补0.5控台输入字符大写输出的类.py
+++ 补充练习/补0.5控台输入字符大写输出的类.py
@@ -3,15 +3,26 @@ import sys
 
 
 class MyString:
-    def __init__(self, stdout):
-        self.stdout = stdout
+    def __init__(self):
+        print('initial...')
 
-    def getString(self,t):
-        self.t = sys.argv[1]
+    def getString(self):
+        print('运行时输入的参数是：', sys.argv[1:])
+        self.lst = sys.argv[1:]
 
-    def printString(self,s):
-        self.stdout.write(s.upper())
+    def printString(self):
+        if len(self.lst) == 1:
+            sys.stdout.write(sys.argv[1].upper())
+        else:
+            new_lst = []
+            for i in self.lst:
+                if i.isalpha():
+                    new_lst.append(i.upper())
+                else:
+                    new_lst.append(i)
+            print('以大写形式输出其中的字母：', new_lst)
 
 
-sys.stdout = MyString(sys.stdout)
-# print(MyString.getString(sys.argv[1])
+a = MyString()
+a.getString()
+a.printString()
